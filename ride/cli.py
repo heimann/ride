dbde877165dee0dda963d081ef1c26a24ea6f5f4
David Heimann <davidheimann@gmail.com>
2018-06-26 18:17:43 -0400

    Add cfonts and pylint

--- ride/cli.py
+++ ride/cli.py
@@ -1,6 +1,15 @@
+from cfonts import render
 from click import (
+    command,
     echo
 )
 
+@command()
 def cli():
-    echo("hello, wanna go for a ride?")
+    echo(
+        render(
+            "Ride.",
+            colors=['blue', 'white'],
+            align='center'
+        )
+    )
